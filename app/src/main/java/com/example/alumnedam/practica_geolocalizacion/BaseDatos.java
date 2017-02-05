26fcff5fcc8e368fbdc66e8988e2ecd692fd230d
BergaAleDAM <titoberguis@gmail.com>
2017-02-05 13:21:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/alumnedam/practica_geolocalizacion/BaseDatos.java
@@ -0,0 +1,62 @@
+package com.example.alumnedam.practica_geolocalizacion;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class BaseDatos extends SQLiteOpenHelper{
+
+    String sqlCreate;
+    String sqlInsert;
+
+    public BaseDatos(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqlCreate= "CREATE TABLE Otobuses (id_bus String, contrasena String)";
+
+        sqlInsert="INSERT INTO Otobuses ('id_bus', 'contrasena') VALUES ('1111A', '1111A')";
+
+        //String fecha = diaActual();
+
+        sqLiteDatabase.execSQL(sqlCreate);
+
+        sqLiteDatabase.execSQL(sqlInsert);
+
+    }
+
+    private String diaActual() {
+
+        Calendar fecha = new GregorianCalendar();
+
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(fecha.get(Calendar.DAY_OF_MONTH));
+        sb.append("/");
+        sb.append(fecha.get(Calendar.MONTH));
+        sb.append("/");
+        sb.append(fecha.get(Calendar.YEAR));
+        sb.append(" ");
+        sb.append(fecha.get(Calendar.HOUR_OF_DAY));
+        sb.append(":");
+        sb.append(fecha.get(Calendar.MINUTE));
+        sb.append(":");
+        sb.append(fecha.get(Calendar.SECOND));
+
+        return sb.toString();
+
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
---
+++ app/src/main/java/com/example/alumnedam/practica_geolocalizacion/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.alumnedam.practica_geolocalizacion;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new BaseDatos(this, "Autobuseros", null, 1);
+
+
+        Button entrar = (Button) findViewById(R.id.acceder);
+        entrar.setOnClickListener(this);
+
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        EditText matri = (EditText) findViewById(R.id.matri);
+        EditText user = (EditText) findViewById(R.id.user);
+        String usuario = user.toString();
+        String matricula = matri.toString();
+
+
+        BaseDatos bd = new BaseDatos(this, "Autobuseros", null, 2);
+        SQLiteDatabase sql= bd.getWritableDatabase();
+        //String dades = matricula;
+        String[] dades =  {usuario};
+        Cursor c = sql.rawQuery("SELECT * FROM Otobuses WHERE id_bus != ? ", new String[]{usuario});
+
+        if (c.moveToFirst()) {
+            do {
+
+                Toast toast1 =
+                        Toast.makeText(getApplicationContext(),
+                                (c.getString(0) + " - " + c.getString(1)), Toast.LENGTH_SHORT);
+
+                toast1.show();
+
+
+
+            } while (c.moveToNext());
+        }
+
+
+
+
+
+
+
+
+    }
+}
